96c55529283fe47293479bc064ca767886f9dc19
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-01-30 06:30:36 -0500

    Move js to Suggests.

--- src/install.libs.R
+++ src/install.libs.R
@@ -5,12 +5,17 @@ jsfiles <- list.files(srcdir, pattern = "[.]js$",
                       recursive = TRUE)
 fontfiles <- list.files(srcdir, pattern = "[.]woff$",
                         recursive = TRUE)
+minify <- requireNamespace("js")
 for (f in jsfiles) {
-  mini <- js::uglify_files(file.path(srcdir, f), unused = FALSE)
   dir <- file.path(destdir, dirname(f))
   if (!dir.exists(dir))
     dir.create(dir, recursive = TRUE)
-  writeLines(mini, file.path(dir, basename(f)))
+  if (minify) {
+    mini <- js::uglify_files(file.path(srcdir, f), unused = FALSE)
+    writeLines(mini, file.path(dir, basename(f)))
+  } else
+    file.copy(file.path(srcdir, f),
+              file.path(destdir, f))
 }
 for (f in fontfiles) {
   dir <- file.path(destdir, dirname(f))
